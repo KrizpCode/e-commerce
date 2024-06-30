1c30c3bfdd95c4c235e01125cb0832b3410918b2
KrizpCode <93johan.e@gmail.com>
2024-06-30 19:37:18 +0200

    UpdateProductRequestDto

---
+++ api/ECommerce.API/DTOs/UpdateProductRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerce.API.DTOs;
+
+public class UpdateProductRequestDto
+{
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public int Price { get; set; }
+}
